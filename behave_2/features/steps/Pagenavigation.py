a286d6fb6897573b5889d22c20ea46611357ac9a
mscheuer92 <mscheuer92@github.com>
2021-04-17 17:42:49 -0400

    Added functions and steps. Not fully functional at this time.

--- behave_2/features/steps/Pagenavigation.py
+++ behave_2/features/steps/Pagenavigation.py
@@ -2,6 +2,7 @@ from behave import given, when, then, step
 from selenium import webdriver
 
 
+
 @given('Chrome browser is launched')
 def launchBrowser(context):
     context.driver = webdriver.Chrome(executable_path="/home/michelle/Desktop/Selenium/Drivers/chromedriver_linux64/chromedriver")
@@ -12,30 +13,37 @@ def openHomepage(context):
 
 
 @when('Find Program is clicked')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When Find Program is clicked')
-
+def find_and_click(context):
+    context.driver.find_element_by_partial_link_text('Programs').click()
+    #text = context.driver.find_element_by_partial_link_text('Program').is_present()
+    #assert text is True
+    #text.click()
 
 @then('I validate that Computer Science & Math hyperlink is present')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I validate that Computer Science & Math hyperlink is present')
-
+def validatePresence(context):
+    csmLink = context.driver.find_element_by_link_text("Computer Science & Math").is_present()
+    assert csmLink is True
 
 @then('I click on the hyper link')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I click on the hyper link')
+def clickCSM(context):
+    context.driver.find_element_by_link_text("Computer Science & Math").click()
 
 
 @then('I search for Computer Science')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I search for Computer Science')
-
+def searchCS(context):
+    CS = context.driver.find_element_by_link_text("Computer Science").is_present()
+    assert CS is True
+    
 
 @then('click on the hyperlink')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then click on the hyperlink')
+    context.driver.find_element_by_link_text("Computer Science").click()
 
 
 @then('I will be on the Computer Science Program Page')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I will be on the Computer Science Program Page')
+def closeBrowser(context):
+    context.browser.close()
+
+
+
+
