92502503f7e57496130adc4daa49b64c3a79a8d3
mscheuer92 <mscheuer92@github.com>
2021-04-22 18:35:14 -0400

    Behave project multiple scenarios and examples

---
+++ behave_3/features/GitHub2.feature
@@ -0,0 +1,30 @@
+#Author: Michelle Scheuer
+
+
+
+Feature: Github showing incorrect password with invalid password parameter
+	
+	Scenario: Failed login attempt to Github
+		Given Chrome browser is launched
+    When Github is loaded
+    And I click on Sign in
+    Then I am taken to the login page
+    And I enter username "mscheuer92" and password "fakepwd"
+    And Click the login button
+    Then I am shown a message saying "Incorrect username or password"
+    
+  Scenario Outline: Login to GitHub with multiple parameters
+    Given Chrome browser is launched
+    When Github is loaded
+    And I click on Sign in
+    Then I am taken to the login page
+   	And I enter username "<username>" and password "<password>"
+  	And Click the login button
+   	Then I am shown a message saying "Incorrect username or password"
+   
+    Examples:
+   		| username   | password   |
+   		| mscheuer   | fakep      |
+   		| mscheuer92 | mscheuer92 |
+   		| lisa			 | smith		  |
+   	
---
+++ behave_3/features/steps/navigation.py
@@ -0,0 +1,45 @@
+from behave import given, when, then, step
+from selenium import webdriver
+
+@given('Chrome browser is launched')
+def initializeWebdriver(context):
+    context.driver = webdriver.Chrome(executable_path="/home/michelle/Desktop/Selenium/Drivers/chromedriver_linux64/chromedriver")
+
+@when('Github is loaded')
+def launchBrowser(context):
+    context.driver.get("https://www.github.com")
+    
+@when('I click on Sign in')
+def clickSignin(context):
+    context.driver.find_element_by_link_text('Sign in').click()
+    context.driver.implicitly_wait(20)
+
+@then('I am taken to the login page')
+def loginPage(context):
+    context.driver.implicitly_wait(20)
+    verify = context.driver.find_element_by_xpath("//div[@id='login']/div/h1").text
+    assert verify == "Sign in to GitHub"
+
+@then('I enter username "{user}" and password "{pwd}"')
+def enterCredentials(context, user, pwd):
+    context.driver.find_element_by_id("login_field").clear()
+    context.driver.find_element_by_id("login_field").send_keys(user)
+    context.driver.find_element_by_id("password").send_keys(pwd)
+
+@then('Click the login button')
+def clickLogin(context):
+    context.driver.find_element_by_name("commit").click()
+    context.driver.implicitly_wait(30)
+
+@then('I am shown a message saying "Incorrect username or password"')
+def verifyMessage(context):
+    context.driver.implicitly_wait(20)
+    try:
+        verify = context.driver.find_element_by_xpath("//div[@id='js-flash-container']/div/div").text
+    except:
+        context.driver.close()
+        assert True, "Incorrect parameter error shown"
+        
+    if verify != "Incorrect username or password.":
+        context.driver.close()
+        assert False, "Test failed"
